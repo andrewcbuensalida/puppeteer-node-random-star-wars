df963df0cb111f64b4cd1db75da2e14fdc1dd964
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-05 01:05:31 -0700

    asdf

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/star
+# #give permission for everything in the express-app directory
+# sudo chmod -R 777 /home/ec2-user/star
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/star
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 
 #Stopping existing node servers
-# echo "Stopping any existing node servers"
-# pkill node
-# npm i forever -g
+echo "Stopping any existing node servers"
+pkill node
+
 echo "stopping forever 1"
 forever stopall
