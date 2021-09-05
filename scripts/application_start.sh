f7475ab1a60868594bf04c2009d5b508419117c3
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-04 19:09:06 -0700

    ec2-user

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user1/express-app1
+sudo chmod -R 777 /home/ec2-user/star
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user1/express-app1
+cd /home/ec2-user/star
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
