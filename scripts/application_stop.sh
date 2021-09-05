8e6fe4acc164166eb90a951539a411f791379e07
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-05 00:11:30 -0700

    addedd nvm to stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+
 #Stopping existing node servers
 # echo "Stopping any existing node servers"
 # pkill node
-npm i forever -g
-echo "stopping forever"
+# npm i forever -g
+echo "stopping forever 1"
 forever stopall
