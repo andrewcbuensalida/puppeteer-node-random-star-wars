6374ddee6df84f7cc3ac587b1f45fa413e281fd6
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-06 07:44:52 -0700

    kitchen sink

--- readme.md.txt
+++ readme.md.txt
@@ -13,4 +13,4 @@ to run puppeteer on node on linux
 https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-on-aws-ec2-instance-running-amazon-linux
 have to install sudo amazon-linux-extras install epel -y
 and sudo yum install -y chromium
-the path to scripts in linux opt/codedeploy-agent/deployment-root/<then some hash>/deployment-archive/scripts
+the path to scripts in linux /opt/codedeploy-agent/deployment-root/430cc14f-3952-4b4a-a230-aed64b38294a/d-SD5HRMFDC/deployment-archive/scripts/
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # #give permission for everything in the express-app directory
-# sudo chmod -R 777 /home/ec2-user/star
+sudo chmod -R 777 /home/ec2-user/star
 
 # #navigate into our working directory where we have all our github files
 cd /home/ec2-user/star
 
 # #add npm and node to path
-# export NVM_DIR="$HOME/.nvm"	
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 
 #Stopping existing node servers
-echo "Stopping any existing node servers"
+echo "Stopping any existing node servers new from vs code"
 pkill node
 
 # echo "stopping forever 1"
